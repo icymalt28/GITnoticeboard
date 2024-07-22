98a7c3928ea4ae8943a81034f4680bd7d49d520f
icymalt28 <125208204+icymalt28@users.noreply.github.com>
2024-07-23 01:22:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Card from "./Card"
+import emojipedia from "./emojipedia";
+
+function Create(emojiterm){
+  return(
+        <Card 
+        name={emojiterm.name}
+       
+        meaning={emojiterm.meaning}
+        
+        body={emojiterm.body}
+
+        />
+  )
+
+}
+
+
+
+function App() {
+  return ( 
+          <div>
+        <h1>
+          <span>Notice board</span>
+        </h1>
+  
+        <dl className="dictionary">
+          {emojipedia.map(Create)}
+        </dl>
+
+  </div>
+
+
+  );
+}
+
+export default App;
